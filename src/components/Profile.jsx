a11721d7eaf237667956731dd1e74ca1528c7347
ihor <kachkamala@gmail.com>
2022-10-01 13:59:25 +0300

    new version 1.0

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Content = () => {
+  return (
+    <div className="content">
+    <div>
+     <img alt="hooo" src="https://img.freepik.com/free-photo/wide-angle-shot-of-a-single-tree-growing-under-a-clouded-sky-during-a-sunset-surrounded-by-grass_181624-22807.jpg?w=2000"></img>
+    </div>
+    <div>
+      ava + des
+    </div>
+    <div>
+      my pos
+      <div>
+        new post
+      </div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  </div>
+  )
+}
+export default Content
